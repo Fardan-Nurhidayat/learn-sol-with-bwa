48558d54b18bd663043eb3aa9d3574787408d20d
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2026-08-27 06:55:15 +0700

    build simple wallet

---
+++ Inheritance.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.34;
+
+contract Animal {
+    function sound() public pure virtual returns(string memory){
+        return "Sound";
+    }
+}
+
+contract Cat is Animal {
+    function sound() public pure override returns(string memory){
+        return "Meow";
+    }
+}
---
+++ SimpleWallet.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.34;
+
+contract SimpleWallet {
+    address public owner ; 
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable{}
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function withDraw(uint amount) public payable {
+        require(msg.sender == owner , "Must be owner fot withdraw");
+        (bool success , ) = payable(msg.sender).call{value : amount}("");
+        require(success , "Withdraw Failed");
+    }
+
+}
